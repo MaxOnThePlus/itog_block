2fe525417c95dab85a7406fd449e4337bb1ad68b
Maxim Andreevich <barygin.maksim@yandex.ru>
2023-03-12 16:04:38 +0300

    cycle

--- Program.cs
+++ Program.cs
@@ -2,3 +2,12 @@
 string strLine = Console.ReadLine(); 
 string[] inArr = strLine.Split(","); 
 string[] outArr = new string[0]; 
+for (int i = 0; i < inArr.Length; i++) 
+{ 
+    if (inArr[i].Length < 4) 
+    { 
+        Array.Resize(ref outArr, outArr.Length + 1); 
+        outArr[outArr.Length-1] = inArr[i]; 
+    } 
+ 
+} 
